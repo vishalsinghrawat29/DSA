31e102a0dc270748b336694825e036c122d7f124
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2023-01-08 10:50:10 +0530

    Output in Java

---
+++ Java Basics/JavaBasics.java
@@ -0,0 +1,18 @@
+/*
+-----Boiler Plate in Java ---- 
+
+public class JavaBasics{
+    public static void main(String[] args) {
+        
+    }
+}
+
+------For Output------
+
+*/
+
+public class JavaBasics{
+    public static void main(String[] args) {
+        System.out.print("Vishal");
+    }
+}
